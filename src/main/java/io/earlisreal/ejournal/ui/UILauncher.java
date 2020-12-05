a63aa07337ce1543d7c31beab42b4df458329878
Earl Savadera <earl.john.savadera@gmail.com>
2020-12-05 18:05:44 +0800

    Add hello world UI

---
+++ src/main/java/io/earlisreal/ejournal/ui/UILauncher.java
@@ -0,0 +1,22 @@
+package io.earlisreal.ejournal.ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class UILauncher extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/scene.fxml"));
+
+        Scene scene = new Scene(root);
+
+        primaryStage.setTitle("eJournal");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+}

e313b08b6bba5b0dfef259421596fa3b449a28e8
Earl Savadera <earl.john.savadera@gmail.com>
2020-12-07 09:51:58 +0800

    Add parseLedger()

--- src/main/java/io/earlisreal/ejournal/input/ConsoleParser.java
+++ src/main/java/io/earlisreal/ejournal/input/ConsoleParser.java
@@ -8,12 +8,26 @@ public class ConsoleParser {
 
     public List<String> parseCsv() {
         System.out.println("Follow this csv format - date (yyyy-mm-dd), stock, action (buy/sell), price, shares, strategy, type (long/short)");
-        System.out.println("Enter csv records:");
-        List<String> records = new ArrayList<>();
+        System.out.println("Enter csv records. Blank line to stop:");
+        return scan(1);
+    }
+
+    public List<String> parseLedger() {
+        System.out.println("Paste the Ledger here with 2 blank lines at the end:");
+        return scan(2);
+    }
+
+    private List<String> scan(int blankLines) {
+        int blank = 0;
         Scanner scanner = new Scanner(System.in);
+
+        List<String> records = new ArrayList<>();
         while (true) {
             String line = scanner.nextLine();
             if (line.isBlank()) {
+                ++blank;
+            }
+            if (blank == blankLines) {
                 break;
             }
 

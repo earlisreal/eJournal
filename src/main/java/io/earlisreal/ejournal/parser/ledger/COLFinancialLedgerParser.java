2349cc21bc94aa63fbc0cb1e70483d4d159e8d9a
Earl Savadera <earl.john.savadera@gmail.com>
2020-12-07 15:07:31 +0800

    Add method for parsing Bank Transactions

--- src/main/java/io/earlisreal/ejournal/parser/ledger/COLFinancialLedgerParser.java
+++ src/main/java/io/earlisreal/ejournal/parser/ledger/COLFinancialLedgerParser.java
@@ -1,41 +1,89 @@
 package io.earlisreal.ejournal.parser.ledger;
 
+import io.earlisreal.ejournal.dto.BankTransaction;
 import io.earlisreal.ejournal.dto.TradeLog;
 
+import java.text.NumberFormat;
+import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class COLFinancialLedgerParser implements LedgerParser {
 
-    COLFinancialLedgerParser() {}
+    private final NumberFormat numberFormat;
 
-    public List<TradeLog> parseAsObjects(List<String> lines) {
-        List<TradeLog> tradeLogs = new ArrayList<>();
+    private List<TradeLog> tradeLogs;
+    private List<BankTransaction> bankTransactions;
+
+    COLFinancialLedgerParser() {
+        numberFormat = NumberFormat.getInstance(Locale.getDefault());
+    }
+
+    public void parse(List<String> lines) {
+        tradeLogs = new ArrayList<>();
+        bankTransactions = new ArrayList<>();
+
         for (int i = 0; i < lines.size(); ++i) {
             if (lines.get(i).contains("TRX DATE")) {
                 for (int j = i + 4; !lines.get(j).startsWith("---"); ++j) {
-                    if (lines.get(j).contains("OR")) {
-                        // TODO : Parse as Bank Transaction
+                    try {
+                        if (!lines.get(j).trim().contains("BUY") && !lines.get(j).trim().contains("SELL")) {
+                            bankTransactions.add(parseBankTransaction(lines.get(j)));
+                        } else {
+                            tradeLogs.add(parseTradeLog(lines.get(j)));
+                        }
                     }
-                    else {
-                        tradeLogs.add(parseTradeLog(lines.get(j)));
+                    catch (ParseException e) {
+                        System.out.println(e.getMessage());
+                        e.printStackTrace();
                     }
+                    ++i;
                 }
-                break;
+                if (lines.get(i).contains("STOCK POS.")) {
+                    break;
+                }
             }
         }
-        return null;
+
+        System.out.println(tradeLogs);
+        System.out.println(bankTransactions);
     }
 
-    private TradeLog parseTradeLog(String line) {
+    public List<TradeLog> getTradeLogs() {
+        return tradeLogs;
+    }
+
+    public List<BankTransaction> getBankTransactions() {
+        return bankTransactions;
+    }
+
+    private BankTransaction parseBankTransaction(String line) throws ParseException {
+        String[] tokens = line.split(":");
+        BankTransaction bankTransaction = new BankTransaction();
+
+        bankTransaction.setDate(LocalDate.parse(tokens[1], DateTimeFormatter.ofPattern("MMdduuuu")));
+            double amount = numberFormat.parse(tokens[5].trim()).doubleValue();
+            bankTransaction.setAmount(amount);
+        if (tokens[2].equals("WFUNDS")) {
+            bankTransaction.setAmount(bankTransaction.getAmount() * -1);
+        }
+        else if (!tokens[2].trim().equals("OR")) {
+            bankTransaction.setDividend(true);
+        }
+
+        return bankTransaction;
+    }
+
+    private TradeLog parseTradeLog(String line) throws ParseException {
         String[] tokens = line.split(":");
         LocalDate date = LocalDate.parse(tokens[1], DateTimeFormatter.ofPattern("MMdduuuu"));
         boolean isBuy = tokens[2].trim().equals("BUY");
         String stock = tokens[4].trim();
-        int shares = Integer.parseInt(tokens[5].trim());
-        double price = Double.parseDouble(tokens[6].trim());
+        int shares = numberFormat.parse(tokens[5].trim()).intValue();
+        double price = numberFormat.parse(tokens[6].trim()).doubleValue();
         return new TradeLog(date, stock, isBuy, price, shares, null, false);
     }
 
--- src/main/java/io/earlisreal/ejournal/parser/ledger/LedgerParser.java
+++ src/main/java/io/earlisreal/ejournal/parser/ledger/LedgerParser.java
@@ -1,11 +1,16 @@
 package io.earlisreal.ejournal.parser.ledger;
 
+import io.earlisreal.ejournal.dto.BankTransaction;
 import io.earlisreal.ejournal.dto.TradeLog;
 
 import java.util.List;
 
 public interface LedgerParser {
 
-    List<TradeLog> parseAsObjects(List<String> lines);
+    void parse(List<String> lines);
+
+    List<TradeLog> getTradeLogs();
+
+    List<BankTransaction> getBankTransactions();
 
 }

601685d3920a13fd4527028e0767ce88cbbe3ddf
Earl Savadera <earl.john.savadera@gmail.com>
2020-12-09 07:10:28 +0800

    Add logic for displaying data from service to ui

--- src/main/java/io/earlisreal/ejournal/ui/controller/MainController.java
+++ src/main/java/io/earlisreal/ejournal/ui/controller/MainController.java
@@ -1,4 +1,29 @@
 package io.earlisreal.ejournal.ui.controller;
 
-public class MainController {
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Tab;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+    public Tab logsTab;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/fxml/logs.fxml"));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+        logsTab.setContent(root);
+    }
+
 }

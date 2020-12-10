a1be9391d49516db6befec5995832cb3c8683da3
Earl Savadera <earl.john.savadera@gmail.com>
2020-12-10 08:37:23 +0800

    Add jMetro as theme

--- src/main/java/io/earlisreal/ejournal/ui/UILauncher.java
+++ src/main/java/io/earlisreal/ejournal/ui/UILauncher.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import jfxtras.styles.jmetro.JMetro;
+import jfxtras.styles.jmetro.Style;
 
 public class UILauncher extends Application {
 
@@ -13,6 +15,8 @@ public class UILauncher extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
 
         Scene scene = new Scene(root);
+        JMetro jMetro = new JMetro(Style.LIGHT);
+        jMetro.setScene(scene);
 
         primaryStage.setTitle("eJournal");
         primaryStage.setScene(scene);

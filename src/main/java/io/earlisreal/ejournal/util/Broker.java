2fa9c76e6d2830d479f2d29ccab41fbf6b4461ad
Earl Savadera <earl.john.savadera@gmail.com>
2020-12-14 08:45:41 +0800

    Add email query string

--- src/main/java/io/earlisreal/ejournal/util/Broker.java
+++ src/main/java/io/earlisreal/ejournal/util/Broker.java
@@ -2,16 +2,30 @@ package io.earlisreal.ejournal.util;
 
 public enum Broker {
 
-    AAA("AAA", System.lineSeparator() + "AAA" + System.lineSeparator()),
-    YAPSTER("2TradeAsia", "YAPSTER e-TRADE"),
-    COL("COL Financial", "COL FINANCIAL GROUP, INC.");
+    AAA(
+            "AAA",
+            System.lineSeparator() + "AAA" + System.lineSeparator(),
+            "subject:(AAA EQUITIES) PDFAPAR has:attachment"
+    ),
+    YAPSTER(
+            "2TradeAsia",
+            "YAPSTER e-TRADE",
+            "from:(accounting@2tradeasia.com) subject:(YAPSTER E-TRADE, INC. (2TRADE ASIA) Invoice) has:attachment"
+    ),
+    COL(
+            "COL Financial",
+            "COL FINANCIAL GROUP, INC.",
+            "from:(alert@colfinancial.com) subject:(COL Trading Confirmation)"
+    );
 
     private final String name;
     private final String uniqueIdentifier;
+    private final String emailFilter;
 
-    Broker(String name, String uniqueIdentifier) {
+    Broker(String name, String uniqueIdentifier, String emailFilter) {
         this.name = name;
         this.uniqueIdentifier = uniqueIdentifier;
+        this.emailFilter = emailFilter;
     }
 
     public String getName() {
@@ -20,6 +34,10 @@ public enum Broker {
 
     public String getUniqueIdentifier() {
         return uniqueIdentifier;
+    }
+
+    public String getEmailFilter() {
+        return emailFilter;
     }
 
 }

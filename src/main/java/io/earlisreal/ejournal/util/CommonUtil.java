1cf804800383e7b80856fee17fb2f01372a8bdf1
Earl Savadera <earl.john.savadera@gmail.com>
2020-12-16 09:06:08 +0800

    Add parseInt() and parseDouble()

--- src/main/java/io/earlisreal/ejournal/util/CommonUtil.java
+++ src/main/java/io/earlisreal/ejournal/util/CommonUtil.java
@@ -1,6 +1,11 @@
 package io.earlisreal.ejournal.util;
 
+import java.text.NumberFormat;
+import java.text.ParseException;
+
 public interface CommonUtil {
+
+    NumberFormat numberFormat = NumberFormat.getNumberInstance();
 
     static void handleException(Exception e) {
         System.out.println(e.getMessage());
@@ -15,6 +20,14 @@ public interface CommonUtil {
         }
 
         throw new RuntimeException("Invoice of this format is not supported");
+    }
+
+    static int parseInt(String text) throws ParseException {
+        return numberFormat.parse(text).intValue();
+    }
+
+    static double parseDouble(String text) throws ParseException {
+        return numberFormat.parse(text).doubleValue();
     }
 
 }

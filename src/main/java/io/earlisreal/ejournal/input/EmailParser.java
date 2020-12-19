214b2db7e177af042560aa08bb8c65302a641c1e
Earl Savadera <earl.john.savadera@gmail.com>
2020-12-19 12:54:30 +0800

    Add getting of last email query time and applying to query

--- src/main/java/io/earlisreal/ejournal/input/EmailParser.java
+++ src/main/java/io/earlisreal/ejournal/input/EmailParser.java
@@ -13,7 +13,8 @@ import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.gmail.Gmail;
 import com.google.api.services.gmail.GmailScopes;
 import com.google.api.services.gmail.model.ListMessagesResponse;
-import io.earlisreal.ejournal.scraper.EmailScrapperFactory;
+import io.earlisreal.ejournal.scraper.EmailAttachmentScraper;
+import io.earlisreal.ejournal.service.ServiceProvider;
 import io.earlisreal.ejournal.util.Broker;
 
 import java.io.File;
@@ -21,6 +22,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -41,17 +43,30 @@ public class EmailParser {
 
     public List<String> parse() {
         List<String> records = new ArrayList<>();
-
         try {
-            for (Broker broker : Broker.values()) {
-                if (broker.getEmailFilter() == null) continue;
-                ListMessagesResponse messageResponse = service.users().messages()
-                        .list(USER).setQ(broker.getEmailFilter()).execute();
-                messageResponse.getMessages().parallelStream().forEach(m -> EmailScrapperFactory.getEmailScraper(broker).scrape(service, m.getId()));
+            String email = service.users().getProfile(USER).execute().getEmailAddress();
+            Instant lastQuery = ServiceProvider.getCacheService().getLastSync(email);
+
+            StringBuilder query = new StringBuilder("(");
+            Broker[] brokers = Broker.values();
+            for (int i = 0; i < Broker.values().length; ++i) {
+                if (brokers[i].getEmailFilter() == null) continue;
+                if (i > 0) query.append(" OR ");
+                query.append("(").append(brokers[i].getEmailFilter()).append(")");
             }
+            query.append(") ").append(" AND after:").append(lastQuery.getEpochSecond());
+
+            ListMessagesResponse messageResponse = service.users().messages().list(USER)
+                    .setMaxResults(10_000L)
+                    .setQ(query.toString())
+                    .execute();
+            EmailAttachmentScraper scraper = EmailAttachmentScraper.getInstance();
+            messageResponse.getMessages().parallelStream().forEach(m -> scraper.scrape(service, m.getId()));
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        // TODO : Get the first message (latest) and update the settings last email sync
 
         return records;
     }
@@ -60,7 +75,7 @@ public class EmailParser {
         InputStream inputStream = getClass().getResourceAsStream("/client_id.json");
         GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(inputStream));
         GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                httpTransport, JSON_FACTORY, clientSecrets, List.of(GmailScopes.GMAIL_READONLY, GmailScopes.GMAIL_LABELS))
+                httpTransport, JSON_FACTORY, clientSecrets, List.of(GmailScopes.GMAIL_READONLY))
                 .setDataStoreFactory(new FileDataStoreFactory(new File("tokens")))
                 .setAccessType("offline")
                 .build();

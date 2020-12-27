abc83e9b59fe7efc74651c6fe4b36e3f854f28dc
Earl Savadera <earl.john.savadera@gmail.com>
2020-12-27 18:40:01 +0800

    Add deposit bank transaction

--- src/main/java/io/earlisreal/ejournal/ui/controller/BankTransactionController.java
+++ src/main/java/io/earlisreal/ejournal/ui/controller/BankTransactionController.java
@@ -2,28 +2,43 @@ package io.earlisreal.ejournal.ui.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class BankTransactionController {
+public class BankTransactionController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        loadBankTransactions();
+    }
 
     public void deposit(ActionEvent event) {
         try {
-            Parent dialog = FXMLLoader.load(getClass().getResource("/fxml/dialog/bank-transaction.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/dialog/bank-transaction.fxml"));
+            Parent dialog = loader.load();
             Scene scene = new Scene(dialog);
             Stage stage = new Stage();
             stage.initModality(Modality.APPLICATION_MODAL);
             stage.setScene(scene);
+            BankTransactionDialogController controller = loader.getController();
+            controller.setBankTransactionController(this);
 
             stage.setTitle("Dialog");
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void loadBankTransactions() {
+        System.out.println("loading");
     }
 
 }
--- src/main/java/io/earlisreal/ejournal/ui/controller/BankTransactionDialogController.java
+++ src/main/java/io/earlisreal/ejournal/ui/controller/BankTransactionDialogController.java
@@ -1,4 +1,54 @@
 package io.earlisreal.ejournal.ui.controller;
 
-public class BankTransactionDialogController {
+import io.earlisreal.ejournal.dto.BankTransaction;
+import io.earlisreal.ejournal.service.BankTransactionService;
+import io.earlisreal.ejournal.service.ServiceProvider;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class BankTransactionDialogController implements Initializable {
+
+    public DatePicker datePicker;
+    public TextField amount;
+
+    private BankTransactionService service;
+    private BankTransactionController bankTransactionController;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        service = ServiceProvider.getBankTransactionService();
+    }
+
+    public void okay(ActionEvent event) {
+        BankTransaction bankTransaction = new BankTransaction();
+        bankTransaction.setAmount(Double.parseDouble(amount.getText()));
+        bankTransaction.setDate(datePicker.getValue());
+        service.insert(List.of(bankTransaction));
+
+
+        closeWindow(event);
+    }
+
+    public void cancel(ActionEvent event) {
+        closeWindow(event);
+    }
+
+    public void setBankTransactionController(BankTransactionController bankTransactionController) {
+        this.bankTransactionController = bankTransactionController;
+    }
+
+    private void closeWindow(ActionEvent event) {
+        Node source = (Node) event.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+        stage.close();
+    }
+
 }

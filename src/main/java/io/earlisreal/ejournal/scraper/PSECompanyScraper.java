2db43e188bf2f1c76af93fa6b346e6350479ea5f
Earl Savadera <earl.john.savadera@gmail.com>
2021-01-11 13:37:22 +0800

    Refactor output to map

--- src/main/java/io/earlisreal/ejournal/scraper/PSECompanyScraper.java
+++ src/main/java/io/earlisreal/ejournal/scraper/PSECompanyScraper.java
@@ -1,34 +1,27 @@
 package io.earlisreal.ejournal.scraper;
 
-import io.earlisreal.ejournal.util.Configs;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PSECompanyScraper {
 
     public static final String URL_SOURCE = "https://edge.pse.com.ph/companyDirectory/search.ax";
 
-    public void saveCompanies() {
-        try {
-            List<String> companies = scrapeCompanies();
-            Files.write(Paths.get(Configs.DATA_DIR, "companies.csv"), companies);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public List<String> scrapeCompanies() throws IOException {
-        List<String> companies = new ArrayList<>();
+    public Map<String, String> scrapeCompanies() {
+        Map<String, String> companies = new HashMap<>();
         int limit = 1;
         for (int page = 1; page <= limit; ++page) {
-            Document document = Jsoup.connect(URL_SOURCE).data("pageNo", String.valueOf(page)).post();
+            Document document;
+            try {
+                document = Jsoup.connect(URL_SOURCE).data("pageNo", String.valueOf(page)).post();
+            } catch (IOException e) {
+                return companies;
+            }
 
             if (page == 1) {
                 String count = document.getElementsByClass("count").text();
@@ -38,15 +31,11 @@ public class PSECompanyScraper {
 
             Elements elements = document.select(".list > tbody > tr > td:nth-child(2) > a");
             elements.forEach(element -> {
-                StringBuilder out = new StringBuilder(element.text());
-
                 String onclick = element.attr("onclick");
                 onclick = onclick.substring(onclick.indexOf('(') + 2, onclick.indexOf("')"));
                 String id = onclick.substring(0, onclick.indexOf('\''));
                 String security = onclick.substring(onclick.lastIndexOf('\'') + 1);
-
-                out.append(',').append(id).append(',').append(security);
-                companies.add(out.toString());
+                companies.put(element.text(), id + "," + security);
             });
         }
 

0225e61baa0791213dd536ebe0484230f8b3075c
Earl Savadera <earl.john.savadera@gmail.com>
2021-01-13 20:38:31 +0800

    Limit the data to 5 days before start and 5 days after end of trade

--- python/plot.py
+++ python/plot.py
@@ -1,4 +1,5 @@
 import sys
+from datetime import datetime
 
 import mplfinance
 import numpy
@@ -8,31 +9,37 @@ if __name__ == "__main__":
     print("hello")
 
     buys = {}
-    for buy in sys.argv[3].split(","):
+    buysinput = sys.argv[3].split(",")
+    for buy in buysinput:
         buys[buy] = True
 
     sells = {}
-    for sell in sys.argv[4].split(","):
+    sellsinput = sys.argv[4].split(",")
+    for sell in sellsinput:
         sells[sell] = True
 
     dataframe = pandas.read_csv(sys.argv[1], names=["Date", "Open", "High", "Low", "Close", "Volume"],
                                 parse_dates=["Date"], index_col=0)
     dataframe.index.name = "Date"
 
-    buymarkers = []
-    sellmarkers = []
+    start = dataframe.index.searchsorted(datetime.fromisoformat(buysinput[0]))
+    end = dataframe.index.searchsorted(datetime.fromisoformat(sellsinput[-1]))
+    dataframe = dataframe[start - 5:end + 5]
+
+    buy_markers = []
+    sell_markers = []
     for index, row in dataframe.iterrows():
         d = str(index.date())
         if d in buys:
-            buymarkers.append(row["Low"])
-            sellmarkers.append(numpy.nan)
+            buy_markers.append(row["Low"])
+            sell_markers.append(numpy.nan)
         elif d in sells:
-            buymarkers.append(numpy.nan)
-            sellmarkers.append(row["High"])
+            buy_markers.append(numpy.nan)
+            sell_markers.append(row["High"])
         else:
-            buymarkers.append(numpy.nan)
-            sellmarkers.append(numpy.nan)
+            buy_markers.append(numpy.nan)
+            sell_markers.append(numpy.nan)
 
-    buylot = mplfinance.make_addplot(buymarkers, type="scatter", marker="$\u21E7$", markersize=200, color="green", panel=0)
-    selllot = mplfinance.make_addplot(sellmarkers, type="scatter", marker="$\u21E9$", markersize=200, color="red", panel=0)
-    mplfinance.plot(dataframe, type="candle", volume=True, addplot=[buylot, selllot], savefig=sys.argv[2])
+    buy_plot = mplfinance.make_addplot(buy_markers, type="scatter", marker="$\u21E7$", markersize=200, color="green")
+    sell_plot = mplfinance.make_addplot(sell_markers, type="scatter", marker="$\u21E9$", markersize=200, color="red")
+    mplfinance.plot(dataframe, type="candle", volume=True, addplot=[buy_plot, sell_plot], savefig=sys.argv[2])

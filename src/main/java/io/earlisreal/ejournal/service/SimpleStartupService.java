601305933155207058c80429f6a5709579d069fa
Earl Savadera <earl.john.savadera@gmail.com>
2021-01-13 19:53:17 +0800

    Use CompletableFuture instead of ExecutorService

--- src/main/java/io/earlisreal/ejournal/service/SimpleStartupService.java
+++ src/main/java/io/earlisreal/ejournal/service/SimpleStartupService.java
@@ -3,8 +3,7 @@ package io.earlisreal.ejournal.service;
 import io.earlisreal.ejournal.scraper.CompanyScraper;
 import io.earlisreal.ejournal.scraper.StockListScraper;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.CompletableFuture;
 
 public class SimpleStartupService implements StartupService {
 
@@ -12,19 +11,15 @@ public class SimpleStartupService implements StartupService {
     private final CompanyScraper companyScraper;
     private final StockService stockService;
 
-    private final ExecutorService executorService;
-
     SimpleStartupService(StockListScraper stockListScraper, CompanyScraper companyScraper, StockService stockService) {
         this.stockListScraper = stockListScraper;
         this.companyScraper = companyScraper;
         this.stockService = stockService;
-
-        executorService = Executors.newCachedThreadPool();
     }
 
     @Override
     public void manageStockList() {
-        executorService.submit(() -> {
+        CompletableFuture.runAsync(() -> {
             stockListScraper.parse();
             var stockMap = stockListScraper.getStockMap();
             if (stockService.getStockCount() != stockMap.size()) {

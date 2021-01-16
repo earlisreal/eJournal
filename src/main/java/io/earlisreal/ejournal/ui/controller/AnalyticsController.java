6ee3b8f4a8302e9ed03dec2f474b93cb1cf9e232
Earl Savadera <earl.john.savadera@gmail.com>
2021-01-16 13:07:28 +0800

    Move Recent trades to dashboard

--- src/main/java/io/earlisreal/ejournal/ui/controller/AnalyticsController.java
+++ src/main/java/io/earlisreal/ejournal/ui/controller/AnalyticsController.java
@@ -33,12 +33,10 @@ public class AnalyticsController implements Initializable {
     public BarChart<Double, String> topLosers;
 
     private AnalyticsService service;
-    private TradeLogService tradeLogService;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         service = ServiceProvider.getAnalyticsService();
-        tradeLogService = ServiceProvider.getTradeLogService();
 
         reload();
     }
@@ -46,70 +44,8 @@ public class AnalyticsController implements Initializable {
     public void reload() {
         service.initialize();
         initializeEquityChart();
-        initializeLastTrade();
-        initializePreviousTrades();
         initializeTopLoser();
         initializeTopWinner();
-    }
-
-    private void initializeLastTrade() {
-        var summaries = tradeLogService.getTradeSummaries();
-        if (summaries.isEmpty()) {
-            lastStock.setText("");
-            lastClosedDate.setText("");
-            lastPosition.setText("");
-            lastHolding.setText("");
-            lastProfit.setText("");
-            return;
-        }
-
-        TradeSummary lastTrade = summaries.get(summaries.size() - 1);
-        lastStock.setText(lastTrade.getStock());
-        lastClosedDate.setText(lastTrade.getCloseDate().toString());
-        lastPosition.setText(prettify(lastTrade.getPosition()));
-        lastHolding.setText(String.valueOf(lastTrade.getTradeLength()));
-        lastProfit.setText(prettify(lastTrade.getProfit()));
-    }
-
-    private void initializePreviousTrades() {
-        var summaries = tradeLogService.getTradeSummaries();
-        var panes = previousTradesBox.getChildren();
-        for (int i = 0; i < Math.min(panes.size(), summaries.size()); ++i) {
-            Pane pane = (Pane) panes.get(i);
-            pane.setVisible(true);
-
-            var labels = pane.getChildren();
-            Label win = (Label) labels.get(0);
-            Label stock = (Label) labels.get(1);
-            Label profit = (Label) labels.get(2);
-
-            TradeSummary summary = summaries.get(summaries.size() - 1 - i);
-            stock.setText(summary.getStock());
-            profit.setText(round(summary.getProfitPercentage()) + "%");
-            boolean isWin = summary.getProfit() > 0;
-            if (isWin) {
-                win.setText("WIN");
-                profit.setTextFill(Paint.valueOf("green"));
-                win.setTextFill(Paint.valueOf("green"));
-            }
-            else {
-                win.setText("LOSS");
-                profit.setTextFill(Paint.valueOf("red"));
-                win.setTextFill(Paint.valueOf("red"));
-            }
-
-            pane.setOnMouseClicked(event -> {
-                try {
-                    UIServiceProvider.getTradeDetailsDialogService().show(summary);
-                } catch (IOException e) {
-                    handleException(e);
-                }
-            });
-        }
-
-        for (int i = 0; i < panes.size() - summaries.size(); ++i) {
-            panes.get(panes.size() - 1 - i).setVisible(false);
-        }
     }
 
     private void initializeEquityChart() {
--- src/main/java/io/earlisreal/ejournal/ui/controller/DashboardController.java
+++ src/main/java/io/earlisreal/ejournal/ui/controller/DashboardController.java
@@ -1,4 +1,101 @@
 package io.earlisreal.ejournal.ui.controller;
 
-public class DashboardController {
+import io.earlisreal.ejournal.model.TradeSummary;
+import io.earlisreal.ejournal.service.ServiceProvider;
+import io.earlisreal.ejournal.service.TradeLogService;
+import io.earlisreal.ejournal.ui.service.UIServiceProvider;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Paint;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import static io.earlisreal.ejournal.util.CommonUtil.*;
+
+public class DashboardController implements Initializable {
+
+    public HBox previousTradesBox;
+    public Label lastClosedDate;
+    public Label lastProfit;
+    public Label lastPosition;
+    public Label lastHolding;
+    public Label lastStock;
+
+    private TradeLogService tradeLogService;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        tradeLogService = ServiceProvider.getTradeLogService();
+        reload();
+    }
+
+    public void reload() {
+        initializeLastTrade();
+        initializePreviousTrades();
+    }
+
+    private void initializeLastTrade() {
+        var summaries = tradeLogService.getTradeSummaries();
+        if (summaries.isEmpty()) {
+            lastStock.setText("");
+            lastClosedDate.setText("");
+            lastPosition.setText("");
+            lastHolding.setText("");
+            lastProfit.setText("");
+            return;
+        }
+
+        TradeSummary lastTrade = summaries.get(summaries.size() - 1);
+        lastStock.setText(lastTrade.getStock());
+        lastClosedDate.setText(lastTrade.getCloseDate().toString());
+        lastPosition.setText(prettify(lastTrade.getPosition()));
+        lastHolding.setText(String.valueOf(lastTrade.getTradeLength()));
+        lastProfit.setText(prettify(lastTrade.getProfit()));
+    }
+
+    private void initializePreviousTrades() {
+        var summaries = tradeLogService.getTradeSummaries();
+        var panes = previousTradesBox.getChildren();
+        for (int i = 0; i < Math.min(panes.size(), summaries.size()); ++i) {
+            Pane pane = (Pane) panes.get(i);
+            pane.setVisible(true);
+
+            var labels = pane.getChildren();
+            Label win = (Label) labels.get(0);
+            Label stock = (Label) labels.get(1);
+            Label profit = (Label) labels.get(2);
+
+            TradeSummary summary = summaries.get(summaries.size() - 1 - i);
+            stock.setText(summary.getStock());
+            profit.setText(round(summary.getProfitPercentage()) + "%");
+            boolean isWin = summary.getProfit() > 0;
+            if (isWin) {
+                win.setText("WIN");
+                profit.setTextFill(Paint.valueOf("green"));
+                win.setTextFill(Paint.valueOf("green"));
+            }
+            else {
+                win.setText("LOSS");
+                profit.setTextFill(Paint.valueOf("red"));
+                win.setTextFill(Paint.valueOf("red"));
+            }
+
+            pane.setOnMouseClicked(event -> {
+                try {
+                    UIServiceProvider.getTradeDetailsDialogService().show(summary);
+                } catch (IOException e) {
+                    handleException(e);
+                }
+            });
+        }
+
+        for (int i = 0; i < panes.size() - summaries.size(); ++i) {
+            panes.get(panes.size() - 1 - i).setVisible(false);
+        }
+    }
+
 }

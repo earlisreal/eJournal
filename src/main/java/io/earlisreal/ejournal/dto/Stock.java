668639e5277679548e880e366766de0cc318e79c
Earl Savadera <earl.john.savadera@gmail.com>
2021-02-04 19:43:11 +0800

    Add Stock DTO

---
+++ src/main/java/io/earlisreal/ejournal/dto/Stock.java
@@ -0,0 +1,74 @@
+package io.earlisreal.ejournal.dto;
+
+import java.time.LocalDate;
+
+public class Stock {
+
+    private String code;
+    private String name;
+    private String companyId;
+    private String securityId;
+    private double price;
+    private LocalDate lastDate;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(String companyId) {
+        this.companyId = companyId;
+    }
+
+    public String getSecurityId() {
+        return securityId;
+    }
+
+    public void setSecurityId(String securityId) {
+        this.securityId = securityId;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public LocalDate getLastDate() {
+        return lastDate;
+    }
+
+    public void setLastDate(LocalDate lastDate) {
+        this.lastDate = lastDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Stock{" +
+                "code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                ", companyId='" + companyId + '\'' +
+                ", securityId='" + securityId + '\'' +
+                ", price=" + price +
+                ", lastDate=" + lastDate +
+                '}';
+    }
+
+}

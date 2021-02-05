0ff8a79509caf670b515d6c6b035078af8da4ebd
Earl Savadera <earl.john.savadera@gmail.com>
2021-02-05 18:31:58 +0800

    Add best and worst trades

--- src/main/java/io/earlisreal/ejournal/ui/controller/AnalyticsController.java
+++ src/main/java/io/earlisreal/ejournal/ui/controller/AnalyticsController.java
@@ -1,5 +1,6 @@
 package io.earlisreal.ejournal.ui.controller;
 
+import io.earlisreal.ejournal.model.TradeSummary;
 import io.earlisreal.ejournal.service.AnalyticsService;
 import io.earlisreal.ejournal.service.ServiceProvider;
 import javafx.collections.FXCollections;
@@ -7,16 +8,24 @@ import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+
+import static io.earlisreal.ejournal.util.CommonUtil.prettify;
 
 public class AnalyticsController implements Initializable {
 
     public LineChart<String, Double> equityChart;
     public BarChart<Double, String> topWinners;
     public BarChart<Double, String> topLosers;
+    public VBox successProfit;
+    public VBox failLoss;
+    public VBox successPercent;
+    public VBox failPercent;
 
     private AnalyticsService service;
 
@@ -32,6 +41,58 @@ public class AnalyticsController implements Initializable {
         initializeEquityChart();
         initializeTopLoser();
         initializeTopWinner();
+        initializeRemarkableTrades();
+    }
+
+    private void initializeRemarkableTrades() {
+        double maxProfit = 0;
+        double maxProfitPercent = 0;
+        double minProfit = 0;
+        double minProfitPercent = 0;
+
+        TradeSummary bestTradeProfit = null;
+        TradeSummary bestTradePercent = null;
+        TradeSummary worstTradeProfit = null;
+        TradeSummary worstTradePercentage = null;
+        for (TradeSummary summary : service.getSummaries()) {
+            double profit = summary.getProfit();
+            double percent = summary.getProfitPercentage();
+            if (profit > maxProfit) {
+                maxProfit = profit;
+                bestTradeProfit = summary;
+            }
+
+            if (percent > maxProfitPercent) {
+                maxProfitPercent = percent;
+                bestTradePercent = summary;
+            }
+
+            double lossPercent = summary.getProfitPercentage();
+            double loss = summary.getProfit();
+            if (lossPercent < minProfitPercent) {
+                worstTradePercentage = summary;
+                minProfitPercent = lossPercent;
+            }
+
+            if (loss < minProfit) {
+                worstTradeProfit = summary;
+                minProfit = loss;
+            }
+        }
+
+        setTrade(successProfit, bestTradeProfit);
+        setTrade(failLoss, worstTradeProfit);
+        setTrade(successPercent, bestTradePercent);
+        setTrade(failPercent, worstTradePercentage);
+    }
+
+    private void setTrade(VBox vBox, TradeSummary tradeSummary) {
+        Label stock = (Label) vBox.getChildren().get(0);
+        stock.setText(tradeSummary.getStock());
+        Label percent = (Label) vBox.getChildren().get(1);
+        percent.setText(prettify(tradeSummary.getProfitPercentage()) + "%");
+        Label value = (Label) vBox.getChildren().get(2);
+        value.setText(prettify(tradeSummary.getProfit()));
     }
 
     private void initializeEquityChart() {

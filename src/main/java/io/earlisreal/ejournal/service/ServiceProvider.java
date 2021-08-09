12c521c672cf014e3c93a0a954783318f5b0da39
Earl Savadera <earl.john.savadera@gmail.com>
2021-08-09 08:03:46 +0800

    Add getIntradayService()

--- src/main/java/io/earlisreal/ejournal/service/ServiceProvider.java
+++ src/main/java/io/earlisreal/ejournal/service/ServiceProvider.java
@@ -1,7 +1,12 @@
 package io.earlisreal.ejournal.service;
 
+import io.earlisreal.ejournal.client.AlphaVantageClient;
+import io.earlisreal.ejournal.client.JsoupAlphaVantageClient;
 import io.earlisreal.ejournal.dao.DAOProvider;
 import io.earlisreal.ejournal.scraper.ScraperProvider;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public final class ServiceProvider {
 
@@ -13,6 +18,7 @@ public final class ServiceProvider {
     private static StartupService startupService;
     private static PlotService plotService;
     private static PlanService planService;
+    private static IntradayService intradayService;
 
     private ServiceProvider() {}
 
@@ -23,7 +29,8 @@ public final class ServiceProvider {
                     // TODO : Remove this circular dependency of ScraperProvider and ServiceProvider
                     startupService = new SimpleStartupService(ScraperProvider.getStockListScraper(),
                             ScraperProvider.getCompanyScraper(), ScraperProvider.getExchangeRateScraper(),
-                            getStockService(), getTradeLogService(), getAnalyticsService(), getCacheService());
+                            getStockService(), getTradeLogService(), getAnalyticsService(), getCacheService(),
+                            ScraperProvider.getUsCompanyScraper());
                 }
             }
         }
@@ -113,6 +120,24 @@ public final class ServiceProvider {
         }
 
         return planService;
+    }
+
+    public static IntradayService getIntradayService() {
+        if (intradayService == null) {
+            synchronized (ServiceProvider.class) {
+                if (intradayService == null) {
+                    String apiKey = System.getenv("AV_API_KEYS");
+                    if (apiKey == null) throw new RuntimeException("AV_API_KEYS not found. Please set Alpha Vantage apiKeys to environment variables (comma separated)");
+                    List<AlphaVantageClient> clients = new ArrayList<>();
+                    for (String key : apiKey.split(",")) {
+                        clients.add(new JsoupAlphaVantageClient(key));
+                    }
+                    intradayService = new AsyncIntradayService(clients);
+                }
+            }
+        }
+
+        return intradayService;
     }
 
 }

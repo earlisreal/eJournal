16c04adc05b9a771288a5f97e03a8c1d464949cb
Earl Savadera <earl.john.savadera@gmail.com>
2021-08-10 08:52:21 +0800

    Add plotIntraday()

--- src/main/java/io/earlisreal/ejournal/service/SimplePlotService.java
+++ src/main/java/io/earlisreal/ejournal/service/SimplePlotService.java
@@ -1,6 +1,8 @@
 package io.earlisreal.ejournal.service;
 
+import com.jsoniter.output.JsonStream;
 import io.earlisreal.ejournal.dto.TradeLog;
+import io.earlisreal.ejournal.model.IntradayPlotArgument;
 import io.earlisreal.ejournal.model.TradeSummary;
 import io.earlisreal.ejournal.scraper.StockPriceScraper;
 import io.earlisreal.ejournal.util.CommonUtil;
@@ -11,6 +13,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.earlisreal.ejournal.util.Configs.plotDirectory;
 import static io.earlisreal.ejournal.util.Configs.stocksDirectory;
@@ -27,7 +32,10 @@ public class SimplePlotService implements PlotService {
 
     @Override
     public Path plot(TradeSummary tradeSummary) throws IOException {
-        // TODO : Check open and close date if same for intra day or swing plot
+        if (tradeSummary.getOpenDate().toLocalDate().equals(tradeSummary.getCloseDate().toLocalDate())) {
+            return plotIntraday(tradeSummary);
+        }
+
         Path imagePath = plotDirectory.resolve(generateImageName(tradeSummary));
         String stock = tradeSummary.getStock();
         LocalDate lastDate = stockService.getLastPriceDate(stock);
@@ -56,20 +64,54 @@ public class SimplePlotService implements PlotService {
             }
         }
 
-        ProcessBuilder processBuilder = new ProcessBuilder("python", "python/scripts/plot.py",
-                stocksDirectory.resolve(stock + ".csv").toString(),
+        run("python", "python/scripts/plot.py", stocksDirectory.resolve(stock + ".csv").toString(),
                 imagePath.toString(), tradeSummary.isClosed() ? "1" : "0", buys.toString(), sells.toString());
-        processBuilder.redirectErrorStream(true);
-        Process process = processBuilder.start();
-        try {
-            int res = process.waitFor();
-            String output = IOUtils.toString(process.getInputStream());
-            System.out.println("plot.py returned " + res);
-            System.out.println(output);
-        } catch (InterruptedException e) {
-            CommonUtil.handleException(e);
+
+        return imagePath;
+    }
+
+    private Path plotIntraday(TradeSummary tradeSummary) throws IOException {
+        Path imagePath = plotDirectory.resolve("intraday").resolve(generateImageName(tradeSummary));
+        IntradayPlotArgument argument = new IntradayPlotArgument();
+        argument.setOutputPath(imagePath.toString());
+        var dataPath = stocksDirectory.resolve(tradeSummary.getCountry().name()).resolve(tradeSummary.getStock());
+        argument.setDataPath(dataPath + ".csv");
+
+        Map<String, Double> buys = new HashMap<>();
+        Map<String, Double> shorts = new HashMap<>();
+        Map<String, Double> sells = new HashMap<>();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu-MM-dd HH:mm:'00'");
+        for (TradeLog log : tradeSummary.getLogs()) {
+            var date = log.getDate();
+            if (date.getSecond() > 0) {
+                date = date.plusMinutes(1);
+            }
+            String dateStr = date.format(formatter);
+            if (log.isBuy()) {
+                buys.put(dateStr, log.getPrice());
+            }
+            else {
+                if (log.isShort()) shorts.put(dateStr, log.getPrice());
+                else sells.put(dateStr, log.getPrice());
+            }
         }
 
+        argument.setBuys(buys);
+        argument.setSells(sells);
+        argument.setShorts(shorts);
+        argument.setStart(tradeSummary.getOpenDate().format(formatter));
+        argument.setEnd(tradeSummary.getCloseDate().format(formatter));
+        String json = JsonStream.serialize(argument);
+
+        StringBuilder args = new StringBuilder();
+        for (Character c : json.toCharArray()) {
+            if (c == '"') {
+                args.append("\\");
+            }
+            args.append(c);
+        }
+
+        run("python", "python/scripts/intraday-plot.py", args.toString());
         return imagePath;
     }
 
@@ -81,6 +123,23 @@ public class SimplePlotService implements PlotService {
         }
 
         return imageId + hash + ".png";
+    }
+
+    private void run(String... command) throws IOException {
+        ProcessBuilder processBuilder = new ProcessBuilder(command);
+        processBuilder.redirectErrorStream(true);
+        Process process = processBuilder.start();
+        try {
+            int res = process.waitFor();
+            String output = IOUtils.toString(process.getInputStream());
+            if (res != 0) {
+                System.out.println("Process returned " + res);
+                System.out.println(output);
+            }
+        } catch (InterruptedException e) {
+            CommonUtil.handleException(e);
+        }
+
     }
 
 }

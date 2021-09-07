f9fdb0e266008e4642ffc3b7a0d613f1b3517435
Earl Savadera <earl.john.savadera@gmail.com>
2021-09-07 15:36:28 +0800

    Add function for displaying latest trading day statistics

--- src/main/java/io/earlisreal/ejournal/ui/controller/DayTradeDashboardController.java
+++ src/main/java/io/earlisreal/ejournal/ui/controller/DayTradeDashboardController.java
@@ -10,9 +10,17 @@ import javafx.scene.chart.PieChart;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.paint.Paint;
 
 import java.net.URL;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Comparator;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+
+import static io.earlisreal.ejournal.util.CommonUtil.prettify;
 
 public class DayTradeDashboardController implements Initializable {
 
@@ -44,11 +52,52 @@ public class DayTradeDashboardController implements Initializable {
         tradeLogService = ServiceProvider.getTradeLogService();
         stockService = ServiceProvider.getStockService();
         analyticsService = ServiceProvider.getAnalyticsService();
-
-        reload();
     }
 
     public void reload() {
+        var summaries = tradeLogService.getTradeSummaries();
+        if (summaries.isEmpty()) {
+            dateLabel.setText("N/A");
+            timeLabel.setText("");
+            profitLabel.setText("");
+            lossLabel.setText("");
+            return;
+        }
+
+        initializeStatistics(summaries);
+        initializeChart();
+        initializeTrades();
+    }
+
+    private void initializeStatistics(List<TradeSummary> summaries) {
+        TradeSummary lastSummary = summaries.get(0);
+        summaries = summaries.stream()
+                .filter(tradeSummary -> tradeSummary.getOpenDate().toLocalDate().equals(lastSummary.getOpenDate().toLocalDate()))
+                .sorted(Comparator.comparing(TradeSummary::getOpenDate))
+                .collect(Collectors.toList());
+
+        dateLabel.setText(lastSummary.getOpenDate().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)));
+        var shortFormatter = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT);
+        String startTime = summaries.get(0).getOpenDate().format(shortFormatter);
+        String endTime = lastSummary.getCloseDate().format(shortFormatter);
+        timeLabel.setText(startTime + " - " + endTime);
+
+        var partition = summaries.stream()
+                .collect(Collectors.partitioningBy(tradeSummary -> tradeSummary.getProfit() >= 0, Collectors.summingDouble(TradeSummary::getProfit)));
+        profitLabel.setText("$" + prettify(partition.get(true)) + " profit");
+        lossLabel.setText("$" + prettify(partition.get(false)) + " loss");
+
+        double total = partition.get(true) + partition.get(false);
+        gainLossLabel.setText("$" + prettify(total));
+        if (total > 0) gainLossLabel.setTextFill(Paint.valueOf("GREEN"));
+        else gainLossLabel.setTextFill(Paint.valueOf("RED"));
+    }
+
+    private void initializeChart() {
+
+    }
+
+    private void initializeTrades() {
 
     }
 

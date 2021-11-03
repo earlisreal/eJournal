612d0a16d863c10b5eaa8dd54c0579d6ecfce9b0
Earl Savadera <earl.john.savadera@gmail.com>
2021-11-03 22:36:24 +0800

    notifyPreloader after start

--- src/main/java/io/earlisreal/ejournal/ui/UILauncher.java
+++ src/main/java/io/earlisreal/ejournal/ui/UILauncher.java
@@ -3,24 +3,29 @@ package io.earlisreal.ejournal.ui;
 import io.earlisreal.ejournal.database.DerbyDatabase;
 import io.earlisreal.ejournal.service.ServiceProvider;
 import javafx.application.Application;
+import javafx.application.Preloader;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class UILauncher extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/main.fxml")));
         Scene scene = new Scene(root);
 
         primaryStage.setTitle("eJournal");
         primaryStage.setScene(scene);
-        primaryStage.show();
         primaryStage.setResizable(false);
-        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/eJournal-logo.png")));
+        primaryStage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/eJournal-logo.png"))));
+        primaryStage.show();
+
+        notifyPreloader(new Preloader.ProgressNotification(1.00));
     }
 
     @Override

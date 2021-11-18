96267d1ad12d0c04d99f8ecda7e4e4043aefe986
Earl Savadera <earl.john.savadera@gmail.com>
2021-11-18 21:04:43 +0800

    Update Buy and Sell Color. Add borderWidth property

--- src/main/java/io/earlisreal/ejournal/model/MarkerData.java
+++ src/main/java/io/earlisreal/ejournal/model/MarkerData.java
@@ -2,14 +2,15 @@ package io.earlisreal.ejournal.model;
 
 public class MarkerData {
 
-    public static final String BUY_COLOR = "#4caf50";
-    public static final String SELL_COLOR = "#e91e63";
+    public static final String BUY_COLOR = "#a5d6a7";
+    public static final String SELL_COLOR = "#f48fb1";
     public static final String SHORT_COLOR = "#2196f3";
 
     private long time;
     private double position;
     private String color;
     private String shape;
+    private int borderWidth;
 
     public long getTime() {
         return time;
@@ -41,6 +42,14 @@ public class MarkerData {
 
     public void setShape(String shape) {
         this.shape = shape;
+    }
+
+    public int getBorderWidth() {
+        return borderWidth;
+    }
+
+    public void setBorderWidth(int borderWidth) {
+        this.borderWidth = borderWidth;
     }
 
 }

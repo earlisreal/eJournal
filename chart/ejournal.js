14137f92d52c3421124db7df85fd44aeb3e31e67
Earl Savadera <earl.john.savadera@gmail.com>
2021-11-20 13:06:19 +0800

    Add css and js for chart

--- chart/ejournal.js
+++ chart/ejournal.js
@@ -1,6 +1,25 @@
 let series = null;
 let volumeSeries = null;
 
+function updateTitle(stock, interval) {
+    titleLegend.innerText = stock + ' - ' + interval;
+}
+
+function updateLegend(price, volume) {
+    const className = price.open >= price.close ? 'greenLegend' : 'redLegend';
+    volumeLegend.innerText = volume;
+    openLegend.innerText = price.open.toFixed(2);
+    high.innerText = price.high.toFixed(2);
+    lowLegend.innerText = price.low.toFixed(2);
+    closeLegend.innerText = price.close.toFixed(2);
+
+    openLegend.className = className;
+    highLegend.className = className;
+    lowLegend.className = className;
+    closeLegend.className = className;
+    volumeLegend.className = className;
+}
+
 function setData(data, volumeData) {
     if (series) {
         chart.removeSeries(series);
@@ -29,4 +48,14 @@ function setData(data, volumeData) {
         },
     });
     volumeSeries.setData(volumeData);
+
+    updateLegend(data[data.length - 1], volumeData[volumeData.length - 1].value);
+
+    chart.subscribeCrosshairMove((param) => {
+        const price = param.seriesPrices.get(series);
+        const volume = param.seriesPrices.get(volumeSeries);
+        if (price) {
+            updateLegend(price, volume);
+        }
+    });
 }

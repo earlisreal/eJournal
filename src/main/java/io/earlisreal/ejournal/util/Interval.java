48f04bdee4f7f3a385df7afccc754bede50025ba
Earl Savadera <earl.john.savadera@gmail.com>
2022-01-10 21:29:39 +0800

    Add value and intraDay fields

--- src/main/java/io/earlisreal/ejournal/util/Interval.java
+++ src/main/java/io/earlisreal/ejournal/util/Interval.java
@@ -2,6 +2,26 @@ package io.earlisreal.ejournal.util;
 
 public enum Interval {
 
-    ONE_MINUTE, FIVE_MINUTE, FIFTEEN_MINUTE, DAILY
+    ONE_MINUTE(1, true),
+    FIVE_MINUTE(5, true),
+    FIFTEEN_MINUTE(15, true),
+    DAILY(1, false),
+    WEEKLY(7, false);
+
+    private final int value;
+    private final boolean intraDay;
+
+    Interval(int value, boolean intrayDay) {
+        this.value = value;
+        this.intraDay = intrayDay;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public boolean isIntraDay() {
+        return intraDay;
+    }
 
 }
